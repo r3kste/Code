0a50ff1a48a00c1519836551e4581caec9774454
r3kste <r3kst@proton.me>
2024-01-12 00:13:27 +0530

    code

--- src/aoc/2015/!template.rs
+++ src/aoc/2015/!template.rs
@@ -6,18 +6,25 @@ fn part2(input_file: &String) -> i32 {
     return 0;
 }
 
+fn part1n2(input_file: &String) -> (i32, i32) {
+    return (0, 0);
+}
+
 fn main() {
     let input_file = std::fs::read_to_string(
         std::path::PathBuf::from(std::env::var("FILE_DIR").unwrap())
             .parent()
             .unwrap()
-            .join(".inputs/day0x.txt"),
+            .join(".inputs/dayX.txt"),
     )
     .expect("File Not Found!");
 
     let _part1ans: i32 = part1(&input_file);
-    println!("Day 0X\nPart 1: {}", _part1ans);
+    println!("Day X\nPart 1: {}", _part1ans);
 
     let _part2ans: i32 = part2(&input_file);
     println!("Part 2: {}", _part2ans);
+
+    let (_part1ans, _part2ans): (i32, i32) = part1n2(&input_file);
+    println!("Day X\nPart 1: {}\nPart 2: {}", _part1ans, _part2ans);
 }
